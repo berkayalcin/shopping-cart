208857489d995ed426f820d34523206a933a7313
Berkay <yalcinberkaycom@gmail.com>
2020-08-19 19:41:21 +0300

    Add Concrete Entity Classes

---
+++ Shopping-Cart/Concrete/AuditedEntity.cs
@@ -0,0 +1,26 @@
+﻿using Shopping_Cart.Abstract;
+using System;
+
+namespace Shopping_Cart.Concrete
+{
+    /// <summary>
+    /// Concrete Audited Entity,Integer Primary Key
+    /// </summary>
+    public abstract class AuditedEntity : AuditedEntity<int>
+    {
+
+    }
+
+    /// <summary>
+    /// Concrete Audited Entity
+    /// </summary>
+    /// <typeparam name="TKey">Primary Key</typeparam>
+    public abstract class AuditedEntity<TKey> : IAuditedEntity<TKey> where TKey : struct
+    {
+        public TKey Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int? CreatorUserId { get; set; }
+    }
+
+}
---
+++ Shopping-Cart/Concrete/FullAuditedEntity.cs
@@ -0,0 +1,30 @@
+﻿using Shopping_Cart.Abstract;
+using System;
+
+namespace Shopping_Cart.Concrete
+{
+    /// <summary>
+    /// Full Audited Entity
+    /// </summary>
+    public class FullAuditedEntity : FullAuditedEntity<int>
+    {
+
+    }
+
+    /// <summary>
+    /// Full Audited Entity
+    /// </summary>
+    /// <typeparam name="TKey">Primary Key</typeparam>
+    public class FullAuditedEntity<TKey> : IFullAuditedEntity<TKey> where TKey : struct
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int? CreatorUserId { get; set; }
+        public DateTime LastModificationTime { get; set; }
+        public int LastModifierId { get; set; }
+        public DateTime DeletionTime { get; set; }
+        public int DeletorUserId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
